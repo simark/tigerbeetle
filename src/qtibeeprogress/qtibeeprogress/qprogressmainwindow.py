40cd0b4e831d5f19158cc998f5ccec72cb51da75
Philippe Proulx <eeppeliteloop@gmail.com>
2014-05-26 00:48:07 -0400

    qtibeeprogress: set main window captions style

--- src/qtibeeprogress/qtibeeprogress/qprogressmainwindow.py
+++ src/qtibeeprogress/qtibeeprogress/qprogressmainwindow.py
@@ -1,4 +1,5 @@
 from PyQt5 import Qt
+from PyQt5 import QtGui
 from qtibeeprogress import utils
 from qtibeeprogress.qprogresswidget import QProgressWidget
 
@@ -29,8 +30,26 @@ class QProgressMainWindow(Qt.QMainWindow, utils.QtUiLoad):
             self._txt_state_providers,
         ]
 
+    def _setup_style(self):
+        palette = QtGui.QPalette()
+        palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor('#888'))
+
+        caption_labels = [
+            self._lbl_begin_time_caption,
+            self._lbl_end_time_caption,
+            self._lbl_cur_time_caption,
+            self._lbl_processed_events_caption,
+            self._lbl_traces_caption,
+            self._lbl_state_changes_caption,
+            self._lbl_state_providers_caption,
+        ]
+
+        for label in caption_labels:
+            label.setPalette(palette)
+
     def _setup_ui(self):
         self._load_ui('main_wnd_progress')
+        self._setup_style()
         self._setup_labels()
         self._setup_edits()
         self._add_progress_widget()
